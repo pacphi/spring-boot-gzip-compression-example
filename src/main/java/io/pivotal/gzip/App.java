3c909942a37d57530c22982fbd51acb82bf7cdc1
Chris Phillipson <cphillipson@pivotal.io>
2020-04-08 15:06:51 -0700

    Upgrade to Spring Boot 2.3.0.M4 * Necessitated some updates to config and specific bean def of InMemoryHttpTraceRepository

--- src/main/java/io/pivotal/gzip/App.java
+++ src/main/java/io/pivotal/gzip/App.java
@@ -1,12 +1,29 @@
 package io.pivotal.gzip;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class App {
+
+	@Configuration
+	static class TraceConfig {
+
+		@Bean
+		HttpTraceRepository httpTraceRepository() {
+			return new InMemoryHttpTraceRepository();
+		}
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+
 }
